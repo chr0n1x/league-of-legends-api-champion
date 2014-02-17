ca3d672776fe82bfc9c5a1e79c2c896c8d488d2d
chr0n1x <heilong24@gmail.com>
2014-02-17 02:08:57 -0500

    barebones Api\Champion

---
+++ src/Riot/Api/Champion.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Riot\Api;
+
+use Riot\Api;
+use Httpful\Request;
+
+class Champion extends Api {
+
+  const VERSION = 'v1.1';
+  const ENDPOINT = 'champion';
+
+  public function defaultAction() {
+
+    return $this->champions();
+
+  } // defaultAction
+
+  public function champions() {
+
+    return $this->request( $this->buildEndpointUri() );
+
+  } // champions
+
+  public function availableRegions() {
+
+    return array( 'br', 'eune', 'euw', 'lan', 'las', 'na' );
+
+  } // availableRegions
+
+} // Champion
